eb45af667ef4e054484c7d5165607fdd2dad6937
m8tan <alt6526@gmail.com>
2026-01-03 19:53:16 +0200

    Base buttons structure

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,73 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Pressable } from "react-native";
 
 export default function Index() {
   return (
     <View style={[harry.container]}>
       <Text style={[harry.title]}>NumberGuesser</Text>
+      <View style={[harry.game]}>
+        <Text style={[harry.number]}>placeholder</Text>
+      </View>
+      <View style={[harry.buttons]}>
+        <Pressable style={[harry.button, {backgroundColor: "#59a76c"}]}>
+          <Text>Start</Text>
+        </Pressable>
+        <Pressable style={[harry.button, {backgroundColor: "#d9534f"}]}>
+          <Text>Stop</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
 
 const harry = StyleSheet.create({
   container: {flex: 1, alignItems: "center", padding: 24},
-  title: {fontSize: 32, fontWeight: "600", marginTop: 20},
+  title: {fontSize: 32, fontWeight: "600", marginTop: 20, marginBottom: 40},
+  game: {borderWidth: 2,
+    borderRadius: 12,
+    paddingVertical: 20,
+    paddingHorizontal: 40,
+    marginBottom: 30,
+  },
+  number: {fontSize: 20, fontWeight: "500"},
+  buttons: {
+    flexDirection: "row",
+    gap: 16,
+    marginBottom: 30,
+  },
+  button: {
+    paddingVertical: 12,
+    paddingHorizontal: 28,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "600",
+  },
+  resultcontainer: {
+    height: 140,
+    marginTop: 10
+  },
+  result: {
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 16,
+    width: "100%",
+    maxWidth: 250,
+    alignItems: "center",
+    
+  },
+  resultText: {
+    fontSize: 18,
+    marginVertical: 2,
+  },
+  close: {
+    color: "#e99e1cff",
+    fontWeight: "500"
+  },
+  perfect: {
+    marginTop: 8,
+    fontWeight: "700",
+    color: "#59a76c",
+  },
 })
