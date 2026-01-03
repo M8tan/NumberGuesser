9cdfb16702f7b456757664e0486faf9c3fc89788
m8tan <alt6526@gmail.com>
2026-01-03 20:28:28 +0200

    Added rtl patch to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,7 @@
 import { Stack } from "expo-router";
-
+import { I18nManager } from "react-native";
+I18nManager.allowRTL(false);
+I18nManager.forceRTL(false);
 export default function RootLayout() {
   return <Stack />;
 }
